7d21bfdf3e36b021e86aa3370f3d740d44659797
Eric Song <ericksong91@gmail.com>
2022-12-19 17:06:10 -0500

    Done with assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,28 @@
 # Your code goes here!
+require 'pry'
+
+
+class Anagram
+
+    attr_accessor :array
+
+    def initialize(word)
+        @word = word
+        @array = []
+    end
+
+    def match(array_compare)
+
+        array_word_orig = @word.chars
+
+        @array = array_compare.filter do |element|
+            element.chars.sort == array_word_orig.sort 
+        end
+
+        pp @array
+    end
+end
+
+listen = Anagram.new("listen")
+
+listen.match(%w[enlists google inlets banana])
